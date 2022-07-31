050eab34cbc28f6e96c583c5185b2e34e3432186
kemaldemirel1999 <kmldemirel99@gmail.com>
2022-07-31 22:54:03 +0300

    Prints the page number in each segment

--- pagingWithSegmentation.c
+++ pagingWithSegmentation.c
@@ -1,4 +1,3 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdlib.h>
@@ -6,9 +5,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <errno.h>
-#include <semaphore.h>
 #include <sched.h>
-#include <pthread_impl.h>
 
 int TLB_HIT = 1;
 int TLB_MISS_WITH_PAGE_FAULT = 100;
@@ -18,29 +15,84 @@ int TLB_MISS_WITHOUT_PAGE_FAULT = 10;
     TLB Replacement policy: FIFO
     Virtual Memory Replacement Policy: LRU Replacement Algorithm
     Segment-page numbers information is sufficient for simulation.
+    Every segment has fixed size (1kB) pages.
+
+    Example of 'size' command response:
+    text        -> 1691
+    data        -> 652
+    bss         -> 4
+    dec         -> 2347
+    hex         -> 92b  
+    filename    -> memory-layout
 */
 
+// Runs with byte values
+
+int text;
+int bss;
+int data;
+
 int main(int argc, char *argv[]){
-    char *text = argv[1];
-    char *data = argv[2];
-    char *bss = argv[3];
-    char *dec = argv[4];
-    char *hex = argv[5];
-    char *fileName = argv[6];
-    printf("Text:%s\n", text);
-    printf("Data:%s\n", data);
-    printf("Bss:%s\n", bss);
-    printf("Dec:%s\n", dec);
-    printf("Hex:%s\n", hex);
-    printf("Filename:%s\n", fileName);
+    
+    readFromFile(argv[1]);
+    int segments[3];
+    segments[0] = text/1024;
+    if(text % 1024 > 0){
+        segments[0] = segments[0] + 1;
+    }
+    segments[1] = data/1024;
+    if(data % 1024 > 0){
+        segments[1] = segments[1] + 1;
+    }
+    segments[2] = bss/1024;
+    if(bss % 1024 > 0){
+        segments[2] = segments[2] + 1;
+    }
+    printf("Number of pages in segment[0]:%d\n",segments[0]);
+    printf("Number of pages in segment[1]:%d\n",segments[1]);
+    printf("Number of pages in segment[2]:%d\n",segments[2]);
+
+    return 0; 
+}
+
+int readFromFile( char *executableFileName){
+    FILE *file;
+    char buffer[100];
+    char command[] = "size ";
+    strcat(command, executableFileName);
+    file = popen(command, "r");
+    if(file == NULL){
+        fputs("POPEN: Failed to execute command\n",stderr);
+        return 1;
+    }
+    else{
+        int row = 0;
+        while(fgets(buffer, 99, file) != NULL){
+            if(row == 0){
+                printf("%s", buffer);
+            }
+            else if(row == 1){
+                char *str = buffer;
+                char * pch;
+                pch = strtok (str," ,.");
+                int i = 0;
+                while (pch != NULL)
+                {
+                    if( i == 0){
+                        text = atoi(pch);
+                    }
+                    else if( i == 1){
+                        data = atoi(pch);
+                    }
+                    else if( i == 2){
+                        bss = atoi(pch);
+                    }
+                    pch = strtok (NULL, " ,.");
+                    i++;
+                }
+            }
+            ++row;
+        }
+    }
     return 0;
-
-
-    // Number of TLB miss and its rate
-
-    // Number of Page fault and its rate
-
-    // Total Time
-
-    // Number of invalid references
 }
